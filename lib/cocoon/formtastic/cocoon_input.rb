b8f373fd5e1a3a7ce99c8918541b71360051b1c3
John Bintz <john@coswellproductions.com>
2012-10-25 15:17:47 -0400

    add the most basic sanity tests for formtastic input

--- lib/cocoon/formtastic/cocoon_input.rb
+++ lib/cocoon/formtastic/cocoon_input.rb
@@ -4,26 +4,32 @@ class CocoonInput
   include ::Formtastic::Inputs::Base
 
   def to_html
-    output = []
+    output = label_html << semantic_fields_for << links
 
-    output << label_html
+    template.content_tag(:li, output.html_safe, wrapper_html_options)
+  end
 
-    output << builder.semantic_fields_for(method) do |fields|
-      if fields.object
-        template.render :partial => "#{method.to_s.singularize}_fields", :locals => { :f => fields }
-      end
-    end
-
-    output << template.content_tag(:div, :class => 'links') do
-      template.link_to_add_association template.t('.add'), builder, method, input_html_options
-    end
-
-    data = wrapper_html_options.merge(:class => 'input cocoon')
+  def wrapper_html_options
+    data = super.merge(:class => 'input cocoon')
     if options[:ordered_by]
       data['data-ordered_by'] = options[:ordered_by]
     end
 
-    template.content_tag(:li, output.join('').html_safe, data)
+    data
+  end
+
+  def semantic_fields_for
+    builder.semantic_fields_for(method) do |fields|
+      if fields.object
+        template.render :partial => "#{method.to_s.singularize}_fields", :locals => { :f => fields }
+      end
+    end
+  end
+
+  def links
+    template.content_tag(:div, :class => 'links') do
+      template.link_to_add_association template.t('.add'), builder, method, input_html_options
+    end
   end
 end
 
