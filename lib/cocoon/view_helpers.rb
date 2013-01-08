c521e0cf3ee1975f133717a07ab979350e3f2c2c
James Zapico <james.zapico@gmail.com>
2013-01-08 00:00:36 -0600

    Quick implementation of button_to functions

--- lib/cocoon/view_helpers.rb
+++ lib/cocoon/view_helpers.rb
@@ -29,6 +29,34 @@ module Cocoon
         hidden_field_tag("#{f.object_name}[_destroy]") + link_to(name, '#', html_options)
       end
     end
+    
+    # this will show a button to remove the current association. This should be placed inside the partial.
+    # either you give
+    # - *name* : the text of the button
+    # - *f* : the form this link should be placed in
+    # - *html_options*:  html options to be passed to link_to (see <tt>link_to</tt>)
+    #
+    # or you use the form without *name* with a *&block*
+    # - *f* : the form this button should be placed in
+    # - *html_options*:  html options to be passed to button_to_function (see <tt>button_to_function</tt>)
+    # - *&block*:        the output of the block will be show in the link, see <tt>button_to_function</tt>
+    
+    def button_to_remove_association(*args, &block)
+      if block_given?
+        f            = args.first
+        html_options = args.second || {}
+        name         = capture(&block)
+        link_to_remove_association(name, f, html_options)
+      else
+        name         = args[0]
+        f            = args[1]
+        html_options = args[2] || {}
+
+        is_dynamic = f.object.new_record?
+        html_options[:class] = [html_options[:class], "remove_fields #{is_dynamic ? 'dynamic' : 'existing'}"].compact.join(' ')
+        hidden_field_tag("#{f.object_name}[_destroy]") + button_to_function(name, '#', html_options)
+      end
+    end
 
     # :nodoc:
     def render_association(association, f, new_object, render_options={}, custom_partial=nil)
@@ -82,6 +110,50 @@ module Cocoon
         html_options[:'data-association-insertion-template'] = CGI.escapeHTML(render_association(association, f, new_object, render_options, override_partial)).html_safe
 
         link_to(name, '#', html_options )
+      end
+    end
+
+    # shows a button that will allow to dynamically add a new associated object.
+    #
+    # - *name* :         the text to show in the button
+    # - *f* :            the form this should come in (the formtastic form)
+    # - *association* :  the associated objects, e.g. :tasks, this should be the name of the <tt>has_many</tt> relation.
+    # - *html_options*:  html options to be passed to <tt>button_to_function</tt> (see <tt>button_to_function</tt>)
+    #          - *:render_options* : options passed to `simple_fields_for, semantic_fields_for or fields_for`
+    #              - *:locals*     : the locals hash in the :render_options is handed to the partial
+    #          - *:partial*        : explicitly override the default partial name
+    #          - *:wrap_object     : !!! document more here !!!
+    #          - *!!!add some option to build in collection or not!!!*
+    # - *&block*:        see <tt>button_to_function</tt>
+
+    def button_to_add_association(*args, &block)
+      if block_given?
+        f            = args[0]
+        association  = args[1]
+        html_options = args[2] || {}
+        button_to_add_association(capture(&block), f, association, html_options)
+      else
+        name         = args[0]
+        f            = args[1]
+        association  = args[2]
+        html_options = args[3] || {}
+
+        render_options   = html_options.delete(:render_options)
+        render_options ||= {}
+        override_partial = html_options.delete(:partial)
+        wrap_object = html_options.delete(:wrap_object)
+        force_non_association_create = html_options.delete(:force_non_association_create) || false
+
+        html_options[:class] = [html_options[:class], "add_fields"].compact.join(' ')
+        html_options[:'data-association'] = association.to_s.singularize
+        html_options[:'data-associations'] = association.to_s.pluralize
+
+        new_object = create_object(f, association, force_non_association_create)
+        new_object = wrap_object.call(new_object) if wrap_object.respond_to?(:call)
+
+        html_options[:'data-association-insertion-template'] = CGI.escapeHTML(render_association(association, f, new_object, render_options, override_partial)).html_safe
+
+        button_to_function(name, '#', html_options )
       end
     end
 
